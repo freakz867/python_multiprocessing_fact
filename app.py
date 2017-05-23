53bcd3ad4a8ac7ffce9daaae0361ab952bd2b7e7
Dmitry <uinos@yandex.ru>
2017-05-23 16:45:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,11 @@
-import random
-import time
-import sys
-random.seed()
-def genList (size):
-    randomList = []
-     
-    #initialize random list with values between 0 and 100
-    for i in range(size):
-        randomList.append(random.randint(0,10))
-         
-    return randomList
-#return the sum of all elements in the list
-#This is the same as "return sum(inList)" but in long form for readability and emphasis
-def sumList(inList):
-    finalSum = 0
-     
-    #iterate over all values in the list, and calculate the cummulative sum
-    for value in inList:
-        finalSum = finalSum + value
-    return finalSum
+#!/usr/bin/python
+
+from math import factorial
+from multiprocessing import Pool
+
+def print_factorial (value):
+    return factorial(value)
 
 if __name__ == '__main__':
-    N = 10000000
-    #mark the start time
-    startTime = time.time()
-    #create a random list of N integers
-    myList = genList (N)
-    finalSum = sumList(myList)
-    #mark the end time
-    endTime = time.time()
-    #calculate the total time it took to complete the work
-    workTime =  endTime - startTime
-         
-    #print results
-    print('The job took %s seconds to complete' % str(workTime))
+    p = Pool(5)
+    print(p.map(print_factorial, [50000, 60000, 70000]))
